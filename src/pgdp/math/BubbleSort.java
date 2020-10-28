85aae61c63ba1f11d77511b05099ef8d3fea55b6
Martin Dunker <ga27kin@mytum.de>
2020-10-28 15:09:08 +0100

    Exercise-Template pushed by Artemis

---
+++ src/pgdp/math/BubbleSort.java
@@ -0,0 +1,11 @@
+package pgdp.math;
+
+import java.util.*;
+
+public class BubbleSort {
+
+    public void performSort(List<Date> input) {
+
+        //TODO: implement
+    }
+}
---
+++ src/pgdp/math/MergeSort.java
@@ -0,0 +1,12 @@
+package pgdp.math;
+
+import java.util.*;
+
+public class MergeSort {
+
+    public void performSort(List<Date> input) {
+
+        //TODO: implement
+    }
+
+}
