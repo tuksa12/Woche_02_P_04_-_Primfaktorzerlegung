6814457b3717b8f30043f6eee075c0c182fca784
martin <ga27kin@mytum.de>
2020-10-28 15:34:28 +0100

    add more template

--- src/pgdp/math/PrimeFactors.java
+++ src/pgdp/math/PrimeFactors.java
@@ -4,6 +4,10 @@ import static pgdp.MiniJava.*;
 
 public class PrimeFactors {
 	public static void main(String[] args) {
+		int n = readInt("Bitte Zahl eingeben:");
 
+		if (n <= 1) {
+			writeConsole("Fehler: n>1 erwartet!");
+		}
 	}
 }
