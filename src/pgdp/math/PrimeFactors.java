cd7ceaa19da70981a283e4b1f589579661a4ccf3
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2020-11-20 11:49:22 +0100

    11:49

--- src/pgdp/math/PrimeFactors.java
+++ src/pgdp/math/PrimeFactors.java
@@ -5,9 +5,18 @@ import static pgdp.MiniJava.*;
 public class PrimeFactors {
 	public static void main(String[] args) {
 		int n = readInt("Bitte Zahl eingeben:");
+		int divisor = 2;
 
 		if (n <= 1) {
-			writeLineConsole("Fehler: n>1 erwartet!");
-		}
+			writeLineConsole("Fehler: n>1 erwartet!");}
+		else
+			while (n >1){
+				if (n % divisor == 0){
+					n = n/divisor;
+					writeConsole(divisor+" ");}
+				else
+					divisor++;
+			}
+
 	}
 }
